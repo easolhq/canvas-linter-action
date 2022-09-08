7b4bd43e7dc690852bfc834ea083b00a0b17691f
Sasha Gerrand <sgerrand@users.noreply.github.com>
2022-09-08 13:13:28 +0100

    Lock to specific gem version
    Blindly installing `easol-canvas` can have unintended consequences, e.g. v3.0.0 now requires Dart Sass.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@
 # This is needed as native dependencies for the ffi gem
 apk add --update build-base libffi-dev
 
-gem install easol-canvas
+gem install easol-canvas --version='2.2.0'
+
 canvas lint
